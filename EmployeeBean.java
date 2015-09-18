a609cf5519b083708e55d26443e0a5c10aae17b0
vinaykolusu <vinaykolusu@users.noreply.github.com>
2015-09-18 14:41:16 -0400

    Create EmployeeBean.java

---
+++ EmployeeBean.java
@@ -0,0 +1,27 @@
+package com.vinaykolusu.springSample.gitSpringCore;
+
+public class EmployeeBean {
+
+	private String eName;
+	private int eId;
+	private String eRole;
+	public String geteName() {
+		return eName;
+	}
+	public void seteName(String eName) {
+		this.eName = eName;
+	}
+	public int geteId() {
+		return eId;
+	}
+	public void seteId(int eId) {
+		this.eId = eId;
+	}
+	public String geteRole() {
+		return eRole;
+	}
+	public void seteRole(String eRole) {
+		this.eRole = eRole;
+	}
+	
+}
