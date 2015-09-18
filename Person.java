61d0a9cb047c5cb04d41729b26e8176b08a5ba9c
vinaykolusu <vinaykolusu@users.noreply.github.com>
2015-09-18 14:40:49 -0400

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,27 @@
+package com.vinaykolusu.springSample.gitSpringCollections.dao;
+
+public class Person {
+	
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	private String address;
+	private String age;
+
+}
